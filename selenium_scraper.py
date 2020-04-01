588d840523f1aa175ab0b3d40f0f52826dcc05a2
andrew-arkhipov <44785877+andrew-arkhipov@users.noreply.github.com>
2020-04-01 16:32:12 -0500

    Add files via upload

--- selenium_scraper.py
+++ selenium_scraper.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import json
 import time
@@ -15,12 +15,21 @@ COURSES = ["2613530", "2617873", "2617884", "2617896", "2617902", "2617909", "26
 
 def read_credentials(filename):
     credentials = json.load(open(filename))
+    print("Using credentials for", credentials['username'])
 
     return credentials['username'], credentials['password']
 
 
 def init_driver(credentials):
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--window-size=1420, 1080')
+    # options.add_argument('--no-sandbox')
+    # options.add_argument('--disable-gpu')
+
+    print('Initializing Chrome driver...')
+
+    driver = webdriver.Chrome(executable_path='./chromedriver', options=options)
     driver.get("https://onramps.instructure.com")
     username = driver.find_element_by_name("j_username")
     username.send_keys(credentials[0])
@@ -28,6 +37,8 @@ def init_driver(credentials):
     password.send_keys(credentials[1])
     submit = driver.find_element_by_xpath("//input[@type='submit']")
     submit.click()
+
+    print('Successfully logged in!')
 
     return driver
 
@@ -38,6 +49,8 @@ def get_assignments(driver, course):
     soup = BeautifulSoup(data, "html.parser")
     scripts = soup.find_all("script")
     uncleaned = scripts[4].get_text()[4396:5259]
+
+    print('Getting assignments...')
 
     assignments = []
     temp = ""
@@ -57,15 +70,30 @@ def is_exam(driver, assignment, course):
     soup = BeautifulSoup(data, "html.parser")
     title = [title for title in soup.find_all("title")][0]
 
-    return "Canvas Exam" == title.get_text()[0:11]
+    if "Canvas Exam Unit 4B" == title.get_text()[0:19]:
+        return True, title.get_text()
+    else:
+        return False, None
 
 
 def get_log(driver):
+    try:
+        WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, "speedgrader_iframe")))
+    except:
+        return driver, [], ""
+
     driver.switch_to.frame(driver.find_element_by_id("speedgrader_iframe"))
     log_link = driver.find_elements_by_xpath("//a[@href]")[5].get_attribute("href")
 
     driver.get(log_link)
-    time.sleep(1.2)
+    time.sleep(1.5)
+
+    '''
+    try:
+        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "ic-Icon icon-empty")))
+    except TimeoutError:
+        return driver, [], "null"
+    '''
 
     logs = driver.find_elements_by_tag_name("li")
 
@@ -79,6 +107,7 @@ def get_log(driver):
             potential.append(log.text[:5])
 
     driver.back()
+    print('Successfully scraped log!')
 
     return driver, potential, name
 
@@ -106,32 +135,54 @@ def main():
     for course in COURSES[1:]:
         assignments = get_assignments(driver, course)
 
-        canvas_exams = []
-        for assignment in assignments:
-            if is_exam(driver, assignment, course):
-                canvas_exams.append(assignment)
+        canvas_exams = {}
+        print('Testing assignments for criteria...')
 
-        for exam in canvas_exams:
+        for assignment in assignments:
+            flag, title = is_exam(driver, assignment, course)
+            if flag:
+                canvas_exams[assignment] = title
+
+        for exam in canvas_exams.keys():
             driver.get(
                 "https://onramps.instructure.com/courses/" + course + "/gradebook/speed_grader?assignment_id=" + exam)
             try:
-                WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "speedgrader_iframe")))
+                WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "speedgrader_iframe")))
             except TimeoutError:
                 continue
 
             num_students = driver.find_elements_by_id("x_of_x_students_frd")
-            print(num_students[0].text)
+            print(num_students[0].text[2:] + ' students detected.')
 
-            for i in range(int(num_students[0].text[3:])):
+            no_log_count = 0
+            for i in range(int(num_students[0].text[2:])):
                 driver, times, name = get_log(driver)
-                if is_cheating(times):
-                    f.write(name + "\n")
+                if name != 'Home':
+                    print('Testing', name + '...')
+                else:
+                    print('Action log not detected.')
 
-                WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "speedgrader_iframe")))
+                '''
+                if not times:
+                    no_log_count += 1
+                if no_log_count == 5:
+                    break
+                '''
+
+                if is_cheating(times):
+                    print(name + ' detected.')
+                    f.write(name + ", " + canvas_exams[exam][0:19] + "\n")
+
+                try:
+                    WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, "speedgrader_iframe")))
+                except:
+                    pass
+
                 driver.find_element_by_id("next-student-button").click()
+
+            print('Finished exam! Moving onto the next course...')
 
     f.close()
 
 
-if __name__ == '__main__':
-    main()
+main()
