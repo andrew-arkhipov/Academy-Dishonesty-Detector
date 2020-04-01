064b76619f06eb6e656cf9f01ca72de3205015a7
andrew-arkhipov <44785877+andrew-arkhipov@users.noreply.github.com>
2020-04-01 16:33:39 -0500

    Update selenium_scraper.py

--- selenium_scraper.py
+++ selenium_scraper.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-# from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import json
 import time
